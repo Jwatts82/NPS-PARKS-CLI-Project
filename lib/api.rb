8c2388787fcd7b093c52ef9e4b6cb4888f9c9a1a
jwatts82 <jessperr182@hotmail.com>
2020-09-29 10:39:26 -0700

    park info

--- lib/api.rb
+++ lib/api.rb
@@ -1,7 +1,7 @@
 class API
 
     def self.get_parks(state_code)
-        binding.pry
+        #binding.pry
         key=ENV["API_KEY"].split('=')[1]
         url="https://developer.nps.gov/api/v1/parks?stateCode=#{state_code}&api_key=#{key}"
         uri=URI(url)
@@ -12,8 +12,20 @@ class API
         end
     end
 
-    def self.get_parks()
-        
+    def self.get_parks(park)
+        key=ENV["API_KEY"].split('=')[1]
+        url="https://developer.nps.gov/api/v1/parks?parkCode=#{park_code}&api_key=#{key}"
+        uri=URI(url)
+        response=Net::HTTP.get(uri)
+        park_info = JSON.parse(response)["data"]
+        park.description = park_info["description"]
+        park.operating_hours = park_info["operatingHours"]
+        park.directions = park_info["directionsUrl"]
+        park.activities = park_info["activities"]
+        park.contacts = park_info["contacts"]
+    end
+    binding.pry
+
 end
 
 
